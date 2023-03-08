55472d162a5ab896e881f6c8b69a9c8a2241ffda
LYkcul <liu.qdcy@foxmail.com>
2023-03-08 19:43:39 +0800

    first commit

---
+++ handler.ts
@@ -0,0 +1,8 @@
+import { HomeHandler } from 'hydrooj/src/handler/home'
+
+async function hitokoto_pro() {
+
+}
+HomeHandler.prototype.hitokoto_pro = async (domainId) => {
+    return await hitokoto_pro();
+}
